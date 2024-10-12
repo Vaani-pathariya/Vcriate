62c2c2323494354f4048d76e84b87c5008de3e73
Vaani <vaanipathariya@gmail.com>
2024-10-12 22:19:26 +0530

    (feat): Adding functions for attempts

--- controllers/attempt.js
+++ controllers/attempt.js
@@ -1,6 +1,35 @@
 const createAttempt=async(req,res)=>{
-    res.status(200).json({message:"Success"})
+    try{
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"Internal Server Error"});
+    }
+}
+const addAttemptedQuestion=async(req,res)=>{
+
+}
+const getAllAttempted= async(req,res)=>{
+
+}
+const getAttemptedQues= async (req,res)=>{
+
+}
+const editAttemptedQues = async(req,res)=>{
+
+}
+const deleteAttemptedQues= async(req,res)=>{
+
+}
+const getScore = async(req,res)=>{
+
 }
 module.exports={
-    createAttempt
+    createAttempt,
+    addAttemptedQuestion,
+    getAllAttempted,
+    getAttemptedQues,
+    editAttemptedQues,
+    deleteAttemptedQues,
+    getScore
 }
--- routes/attempt.js
+++ routes/attempt.js
@@ -1,10 +1,22 @@
 const express = require("express");
-const { createAttempt } = require("../controllers/attempt");
+const {
+  createAttempt,
+  addAttemptedQuestion,
+  getAllAttempted,
+  getAttemptedQues,
+  editAttemptedQues,
+  deleteAttemptedQues,
+  getScore,
+} = require("../controllers/attempt");
 const { authenticateToken } = require("../middlewares");
 const router = express.Router();
-router.use(authenticateToken)
-router.route("/attempt").post().get();
-router.route("/attempt/:id").post();
-router.route("/attempt/:id/:ques").get().patch().delete()
-router.route("/score/:id").get();
-module.exports=router
+router.use(authenticateToken);
+router.route("/attempt").post(createAttempt);
+router.route("/attempt/:id").post(addAttemptedQuestion).get(getAllAttempted);
+router
+  .route("/attempt/:id/:ques")
+  .get(getAttemptedQues)
+  .patch(editAttemptedQues)
+  .delete(deleteAttemptedQues);
+router.route("/score/:id").get(getScore);
+module.exports = router;
